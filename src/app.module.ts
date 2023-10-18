3199884af29b7a5fa901ce964a65629c74e35dde
GreatEki <ekeneidiagbor@gmail.com>
2023-10-18 14:27:25 +0100

    chore: prisma config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, AuthModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
