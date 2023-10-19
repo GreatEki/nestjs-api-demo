59210c9edaf8a564955b04b44dcf97996b6387e4
GreatEki <ekeneidiagbor@gmail.com>
2023-10-19 13:04:46 +0100

    chore: impl tokenization strategies

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { AcceessTokenStrategy, RefreshTokenStrategy } from './strategies';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, AcceessTokenStrategy, RefreshTokenStrategy],
 })
 export class AuthModule {}
